d6ff613d1180e5ab0491d7c475da95bb5bf55074
wanwqing@163.com <wanwqing@163.com>
2021-02-06 21:25:56 +0800

    '初始化项目'

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <TestView1></TestView1>
+  </div>
+
+</template>
+
+<script>
+import TestView1 from "./components/test1"
+export default {
+  name: 'App',
+  components: {
+    TestView1
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/test1.vue
@@ -0,0 +1,13 @@
+<template>
+    <div><h1>Test1</h1></div>
+</template>
+
+<script>
+    export default {
+        name: "test1"
+    }
+</script>
+
+<style scoped>
+
+</style>
